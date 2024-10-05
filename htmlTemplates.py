de2a737c773929aeb761b0ca145be6ca6d31fa55
MSITE.TOP <admin@msite.top>
2024-10-05 20:51:38 +0000

    добавляем выбор типа документа

---
+++ htmlTemplates.py
@@ -0,0 +1,3 @@
+links = """
+<style>.stAppHeader {display:none!important}</style>
+"""
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import hmac
 import streamlit as st
 from openai import OpenAI
+from htmlTemplates import links
 
 def check_password():
     """Returns `True` if the user had the correct password."""
@@ -27,10 +28,11 @@ def check_password():
 
 def stream_data():
     with client.beta.threads.runs.stream(
-    thread_id=thread.id,
-    assistant_id="asst_nB18mkuiU34T645GttfB9Dpl",
+        thread_id=thread.id,
+        assistant_id="asst_nB18mkuiU34T645GttfB9Dpl",
     ) as stream:
         for event in stream:
+            #print(event)
             # Print the text from text delta events
             if event.event == "thread.message.delta" and event.data.delta.content:
                 #print(event.data.delta.content[0].text)
@@ -40,6 +42,7 @@ def stream_data():
 if not check_password():
     st.stop()  # Do not continue if check_password is not True.
 
+st.markdown(links, unsafe_allow_html=True)
 # Show title and description.
 st.title("📄 Ответы по документу")
 st.write("Загрузите документ ниже и задайте по нему вопрос – GPT ответит! ")
@@ -47,52 +50,67 @@ st.write("Загрузите документ ниже и задайте по н
 # Create an OpenAI client.
 client = OpenAI(api_key=st.secrets["KEY"])
 
-# Let the user upload a file via `st.file_uploader`.
-uploaded_files = st.file_uploader(
-    "Загрузите документы", accept_multiple_files = True, type=(".c", ".cpp", ".css", ".doc", ".docx", ".go", ".html", ".java", ".js", ".json", ".md", ".pdf", ".php", ".pptx", ".py", ".rb", ".sh", ".ts", ".txt")
+
+type = st.radio(
+    "Выбери тип документа",
+    ["Аудио/Видео", "Текст"],
+    index = None
 )
 
-if uploaded_files:
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-        st.session_state.attach = []
-        for uploaded_file in uploaded_files:
-            message_file = client.files.create(file=uploaded_file, purpose="assistants")
-            st.session_state.attach.append({ 
-                "file_id": message_file.id, 
-                "tools": [{"type": "file_search"}] 
+if type == "Аудио/Видео":
+    uploaded_files = st.file_uploader(
+        "Загрузи файл", type=(".webm", ".aac", ".mov", ".ac3", ".mp2", ".aif", ".mp4", ".m4p", ".m4v", ".aiff", ".flac", ".flv", ".m4a", ".mp3", ".mpga", ".ogg", ".wav", ".wma")
+    )
+    
+elif type == "Текст":
+    # Let the user upload a file via `st.file_uploader`.
+    uploaded_files = st.file_uploader(
+        "Загрузи документы", accept_multiple_files = True, type=(".c", ".cpp", ".css", ".doc", ".docx", ".go", ".html", ".java", ".js", ".json", ".md", ".pdf", ".php", ".pptx", ".py", ".rb", ".sh", ".ts", ".txt")
+    )
+
+    if uploaded_files:
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+            st.session_state.attach = []
+            for uploaded_file in uploaded_files:
+                message_file = client.files.create(file=uploaded_file, purpose="assistants")
+                st.session_state.attach.append({ 
+                    "file_id": message_file.id, 
+                    "tools": [{"type": "file_search"}] 
+                })
+
+        # Display the existing chat messages via `st.chat_message`.
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        # Create a chat input field to allow the user to enter a message. This will display
+        # automatically at the bottom of the page.
+        if prompt := st.chat_input("Задай вопросы по документу"):
+            # Store and display the current prompt.
+            st.session_state.messages.append({
+                "role": "user", 
+                "content": prompt, 
+                "attachments": st.session_state.attach
             })
+            with st.chat_message("user"):
+                st.markdown(prompt)
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+            thread = client.beta.threads.create(
+                messages=[
+                    {"role": m["role"], "content": m["content"], "attachments": m["attachments"]}
+                    for m in st.session_state.messages
+                ]
+            )
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("Задай вопросы по документу"):
-        # Store and display the current prompt.
-        st.session_state.messages.append({
-            "role": "user", 
-            "content": prompt, 
-            "attachments": st.session_state.attach
-        })
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        thread = client.beta.threads.create(
-            messages=[
-                {"role": m["role"], "content": m["content"], "attachments": m["attachments"]}
-                for m in st.session_state.messages
-            ]
-        )
-
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream_data)
-        st.session_state.messages.append({
-            "role": "assistant", 
-            "content": response, 
-            "attachments": []
-        })
+            # Stream the response to the chat using `st.write_stream`, then store it in 
+            # session state.
+            with st.chat_message("assistant"):
+                response = st.write_stream(stream_data)
+            st.session_state.messages.append({
+                "role": "assistant", 
+                "content": response, 
+                "attachments": []
+            })
+        
+    
