de2462316f27af04ab896e0138de35c76813c408
MSITE.TOP <admin@msite.top>
2024-10-17 05:41:02 +0000

    fix

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,6 +1,12 @@
 links = """
 <style>
 .stAppHeader {display:none!important}
-.st-key-upload_file {position: absolute; bottom:0;}
+.st-key-upload_file {
+    position: sticky;
+    left: 0px;
+    bottom: 0px;
+    width: 100%;
+    z-index: 999;
+}
 </style>
 """
